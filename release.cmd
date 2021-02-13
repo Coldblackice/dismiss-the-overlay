9ec3382dcd48038fd641cdf4e1e7e7a8039f09bf
JustOff <Off.Just.Off@gmail.com>
2021-02-14 00:00:54 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,12 @@
+@echo off
+set VER=1.0.3
+
+sed -i -E "s/\"version\": \".+?\"/\"version\": \"%VER%\"/; s/\"name\": \".+?\"/\"name\": \"dismiss-the-overlay-%VER%\"/" package.json
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/dismiss-the-overlay-.+?\.xpi/download\/%VER%\/dismiss-the-overlay-%VER%\.xpi/" update.xml
+
+set XPI=dismiss-the-overlay-%VER%.xpi
+if exist %XPI% del %XPI%
+if exist bootstrap.js del bootstrap.js
+if exist install.rdf del install.rdf
+call jpm xpi
+unzip %XPI% bootstrap.js install.rdf
