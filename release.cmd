b80bf82e7233fa5aeea43efcb456130a0a34429c
JustOff <Off.Just.Off@gmail.com>
2021-02-14 00:04:33 +0200

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.0.4
+set VER=1.0.5
 
 sed -i -E "s/\"version\": \".+?\"/\"version\": \"%VER%\"/; s/\"name\": \".+?\"/\"name\": \"dismiss-the-overlay-%VER%\"/" package.json
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/dismiss-the-overlay-.+?\.xpi/download\/%VER%\/dismiss-the-overlay-%VER%\.xpi/" update.xml
